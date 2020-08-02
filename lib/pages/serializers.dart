3f0118b3ae575ed8a241ab46604479178deddcab
Noel <14931745+FireRedNinja@users.noreply.github.com>
2020-08-02 16:09:20 +0100

    Refactor, cleaning up, caching, allowing more than 10 items to be shown. (#2)

--- lib/pages/serializers.dart
+++ lib/pages/serializers.dart
@@ -7,7 +7,7 @@ library serializers;
 import 'package:built_value/serializer.dart';
 import 'package:built_collection/built_collection.dart';
 import 'package:built_value/standard_json_plugin.dart';
-import 'Item.dart';
+import 'HackerNews/Data/Item.dart';
 
 part 'serializers.g.dart';
 
